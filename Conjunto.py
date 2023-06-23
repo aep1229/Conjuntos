97929d210d13f7507e5b0a08d157571b43cb20c7
aep1229 <136847393+aep1229@users.noreply.github.com>
2023-06-23 15:54:52 -0300

    Add files via upload

---
+++ Conjunto.py
@@ -0,0 +1,29 @@
+"""
+Programa conjuntos
+Descrição: Este programa serve para apresentar conjuntos
+Autora:
+Data: 23/06/23
+Versão: 0.0.1
+"""
+
+# Atribuição de variáveis
+
+lista = [1,2,3,3,5,7,9,11,11,8,8,47]
+conjunto1 = set(lista)
+
+conjunto2 = {1, 3, 4, 3, 6, -7, 8, 4, 7, -5, 0, 0}
+
+# Entrada de dados
+
+# Processamento de dados
+
+conjunto2.add(49)
+
+conjunto2.remove(49)
+
+# Saída de dados
+
+print(conjunto1)
+
+print(conjunto2)
+
